b465e46aa0c549ed61a1b60c434f7d1f7650c2e7
OXOYO <zmn2007.hi@163.com>
2018-02-06 14:59:55 +0800

    remove utils.

--- untils/common_define.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'cwh'
-
-CODE_FILE_EXT_LIST = ['.py', '.c', '.cpp', '.java', '.h',  '.txt',
-                      '.gradle', 'make', '.config', 'CMakeLists.txt', 'Makefile',
-                      '.xml',
-                      '.html', '.css', '.aspx', '.jsp', '.js'
-                      '.cs',  '.ui',   # '.resx', '.resX',
-                      '.m',   # '.pro'
-                      ]
